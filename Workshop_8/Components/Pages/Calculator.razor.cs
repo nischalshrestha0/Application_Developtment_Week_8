5f68fffa12d511e95287099400407ab7da54b2f5
Nischal <nischals054@gmail.com>
2025-12-18 10:03:49 +0545

    workshop8

---
+++ Workshop_8/Components/Pages/Calculator.razor.cs
@@ -0,0 +1,17 @@
+﻿namespace Workshop_8.Components.Pages;
+
+public partial class Calculator
+{
+    public double Num1 { get; set; }
+    public double Num2 { get; set; }
+    public double Result { get; set; }
+
+    public void Add() => Result = Num1 + Num2;
+    public void Subtract() => Result = Num1 - Num2;
+    public void Multiply() => Result = Num1 * Num2;
+
+    public void Divide()
+    {
+        Result = Num2 == 0 ? 0 : Num1 / Num2;
+    }
+}
---
+++ Workshop_8/Components/Pages/Product.cs
@@ -0,0 +1,9 @@
+namespace Workshop_8.Components.Pages;
+
+public class Product
+{
+    public string Name { get; set; } = "";
+    public string ImageUrl { get; set; } = "";
+    public decimal Price { get; set; }
+    public int Likes { get; set; }
+}
